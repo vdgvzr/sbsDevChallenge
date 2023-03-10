d0bc9e7ded4fd03eaffddc1154a5f14ac9266e1c
Shaun Lindsley <shaunlindsley@gmail.com>
2023-03-10 14:47:44 +0000

    API axios request successful on frotend

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class App extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-
+            site: null,
+            page: null
         }
+
+        this.path = window.location.pathname;
+        this.query = window.location.search;
     }
 
     componentDidMount() {
+        /* Get site globals */
+        axios.get("/site.json").then((response) => {
+            console.log(response.data);
+            const site = response.data;
+            this.setState({ site });
+        }).catch((error) => {
+            console.error(error.message);
+        });
 
+        /* Get singles */
+        axios.get(this.path + '.json' + this.query).then((response) => {
+            console.log(response.data);
+            const page = response.data;
+            this.setState({ page });
+        }).catch((error) => {
+            console.error(error.message);
+        });
     }
 
     render() {
