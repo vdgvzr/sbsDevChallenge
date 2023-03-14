faf0b7b230eb491ef934581ac2e54bba393bcc31
Shaun Lindsley <shaunlindsley@gmail.com>
2023-03-14 17:57:19 +0000

    Added flickity to products carousel

--- src/components/components/contentMain/ProductsCarousel.js
+++ src/components/components/contentMain/ProductsCarousel.js
@@ -1,15 +1,30 @@
-import React, { useRef } from "react"
+import React, { useRef, useEffect } from "react"
 import Heading from "../ui/Heading"
 import CarouselButton from "../ buttons/CarouselButton"
 import ProductsCard from "../cards/ProductsCard"
+import Flickity from 'react-flickity-component'
 
 const ProductsCarousel = ({ contentMain }) => {
     const carousel = useRef(null);
     const card = useRef(null);
+    var flkty = null
 
     const scroll = (scrollOffset) => {
         carousel.current.scrollLeft += scrollOffset;
     };
+
+    const flickityOptions = {
+        contain: true,
+        prevNextButtons: false,
+    }
+
+    const myCustomPrev = () => {
+        flkty.previous()
+    }
+
+    const myCustomNext = () => {
+        flkty.next()
+    }
 
     const productsCarousel = contentMain[1].productscarousel
     return(
@@ -22,28 +37,32 @@ const ProductsCarousel = ({ contentMain }) => {
                                 <div className="d-flex products-carousel__header justify-content-between align-items-center pt-lg-0 pt-3">
                                     <Heading heading={productsCarousel.heading} margin={false} />
                                     <div className="d-flex align-items-center products-carousel__button-container">
-                                        <CarouselButton onClick={() => scroll(-card.current.offsetWidth)} icon="chevron-left" />
-                                        <CarouselButton onClick={() => scroll(card.current.offsetWidth)} icon="chevron-right" />
+                                        <CarouselButton onClick={myCustomPrev} icon="chevron-left" />
+                                        <CarouselButton onClick={myCustomNext} icon="chevron-right" />
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="container-fluid h-scrollable p-0 py-5" ref={carousel}>
-                    <div className="container">
-                        <div className="row d-block">
+                <div className="container">
+                    <Flickity
+                        flickityRef={c => (flkty = c)}
+                        options={flickityOptions} // takes flickity options {}
+                        disableImagesLoaded={false} // default false
+                        reloadOnUpdate // default false
+                        static // default false
+                    >
                             {productsCarousel.products.map((product, i) => {
                                 return(
-                                    <div key={i} className="col-lg-3 col-10" ref={card}>
+                                    <div key={i} className="mx-3" ref={card}>
                                         <a href={product.productUrl}>
                                             <ProductsCard product={product}/>
                                         </a>
                                     </div>
                                 )
                             })}
-                        </div>
-                    </div>
+                    </Flickity>
                 </div>
             </div>
         </>
@@ -51,3 +70,41 @@ const ProductsCarousel = ({ contentMain }) => {
 }
 
 export default ProductsCarousel
+
+
+{/* return(
+    <>
+        <div className="products-carousel">
+            <div className="container-fluid p-0">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                            <div className="d-flex products-carousel__header justify-content-between align-items-center pt-lg-0 pt-3">
+                                <Heading heading={productsCarousel.heading} margin={false} />
+                                <div className="d-flex align-items-center products-carousel__button-container">
+                                    <CarouselButton onClick={() => scroll(-card.current.offsetWidth)} icon="chevron-left" />
+                                    <CarouselButton onClick={() => scroll(card.current.offsetWidth)} icon="chevron-right" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="container-fluid h-scrollable p-0 py-5" ref={carousel}>
+                <div className="container">
+                    <div className="row d-block">
+                        {productsCarousel.products.map((product, i) => {
+                            return(
+                                <div key={i} className="col-lg-3 col-10" ref={card}>
+                                    <a href={product.productUrl}>
+                                        <ProductsCard product={product}/>
+                                    </a>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+) */}
