cdcd509341e0d68be7cf2aaff8d98d96e6ffc1d0
Shaun Lindsley <shaunlindsley@gmail.com>
2023-03-14 20:17:27 +0000

    Flickity for partner panel

--- src/components/components/contentMain/PartnerPanel.js
+++ src/components/components/contentMain/PartnerPanel.js
@@ -1,23 +1,26 @@
 import React, { useEffect, useRef } from "react"
 import Heading from "../ui/Heading"
 import Image from "../ui/Image"
+import Flickity from 'react-flickity-component'
 
 const PartnerPanel = ({ contentMain }) => {
     const partnerPanel = contentMain[5].partnerPanel
-    const carouselSlider = useRef(null)
-    const list = useRef(null)
-    const items = useRef([])
+    let flkty
 
-    useEffect(() => {
-        let width = 0
-        items.current.forEach(item => {
-            let clone = item.cloneNode(true)
-            list.current.append(clone)
-            width += item.clientWidth
-            list.current.style.width = `${width}`
-        })
-    
-    }, [])
+    let partners = partnerPanel.logos
+
+    let duplicatePartners = [...partnerPanel.logos];
+    duplicatePartners.forEach(partner => {
+        partners.push(partner)
+    })
+
+    const flickityOptions = {
+        contain: true,
+        pageDots: false,
+        prevNextButtons: false,
+        wrapAround: true,
+        autoPlay: 1500
+    }
 
     return(
         <>
@@ -29,18 +32,22 @@ const PartnerPanel = ({ contentMain }) => {
                         </div>
                     </div>
                 </div>
-                <div className="partner-panel__carousel-container" ref={carouselSlider}> {/* slider */}
-                    <div className="container-fluid partner-panel__carousel px-0" ref={list}> {/* slide track */}
-                        {[...Array(partnerPanel.logos.length)].map((logo, i) => {
+                <div className="container-fluid p-0">
+                    <Flickity
+                        flickityRef={c => flkty = c}
+                        options={flickityOptions}
+                        disableImagesLoaded={false}
+                        reloadOnUpdate
+                        static
+                    >
+                        {[...Array(partners.length)].map((logo, i) => {
                             return(
-                                <div key={i} className="partner-panel__carousel-item" ref={ref=>items.current.push(ref)}>
-                                    <Image src={partnerPanel.logos[i].logo} width="100" justify="center" padding="3" />
+                                <div key={i} className="partner-panel__carousel-item">
+                                    <Image src={partners[i].logo} width="100" justify="center" padding="3" />
                                 </div>
                             )
                         })}
-                    </div>
-                </div>
-                <div className="container d-block d-lg-none mt-5">
+                    </Flickity>
                 </div>
             </div>
         </>
@@ -48,3 +55,46 @@ const PartnerPanel = ({ contentMain }) => {
 }
 
 export default PartnerPanel
+
+/* const partnerPanel = contentMain[5].partnerPanel
+const carouselSlider = useRef(null)
+const list = useRef(null)
+const items = useRef([])
+
+useEffect(() => {
+    let width = 0
+    items.current.forEach(item => {
+        let clone = item.cloneNode(true)
+        list.current.append(clone)
+        width += item.clientWidth
+        list.current.style.width = `${width}`
+    })
+
+}, [])
+
+return(
+    <>
+        <div className="partner-panel">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 mb-5">
+                        <Heading heading={partnerPanel.heading} />
+                    </div>
+                </div>
+            </div>
+            <div className="partner-panel__carousel-container" ref={carouselSlider}>
+                <div className="container-fluid partner-panel__carousel px-0" ref={list}>
+                    {[...Array(partnerPanel.logos.length)].map((logo, i) => {
+                        return(
+                            <div key={i} className="partner-panel__carousel-item" ref={ref=>items.current.push(ref)}>
+                                <Image src={partnerPanel.logos[i].logo} width="100" justify="center" padding="3" />
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+            <div className="container d-block d-lg-none mt-5">
+            </div>
+        </div>
+    </>
+                ) */
